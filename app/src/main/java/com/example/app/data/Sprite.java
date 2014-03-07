f1e8db2224beb2dd56f0de436c7b4b84a84d9b64
giyeophwang <giyeop.hwang@gmail.com>
2014-03-07 16:30:04 -0500

    commit
    commit

--- app/src/main/java/com/example/app/data/Sprite.java
+++ app/src/main/java/com/example/app/data/Sprite.java
@@ -7,17 +7,16 @@ import android.graphics.Paint;
  * Created by John on 3/1/14.
  */
 public class Sprite {
+
     private SpriteType spriteType;
     private float x;
     private float y;
-
 
     public Sprite(SpriteType spriteType, float xPercent, float yPercent){
         this.spriteType = spriteType;
         x = DataModel.getDataModel().toAbsoluteWidth(xPercent);
         y = DataModel.getDataModel().toAbsoluteHeight(yPercent);
     }
-
 
     public void drawSelf(Canvas c){
         c.drawBitmap(spriteType.getImage(), DataModel.getDataModel().getTransX() + x, DataModel.getDataModel().getTransY() + y, new Paint());
